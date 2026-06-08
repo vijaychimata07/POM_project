1e2549e395f29e951d69b598f8b011b98296aea4
vijaychimata07 <vijaychimata07@gmail.com>
2026-06-08 16:40:09 +0530

    updated

--- src/test/java/Tests/LoginTest1.java
+++ src/test/java/Tests/LoginTest1.java
@@ -1,13 +1,16 @@
 package Tests;
 
+import org.testng.annotations.Test;
+
 import Base.BaseClass1;
 import Pages.LoginPage1;
 
 public class LoginTest1 extends BaseClass1{
+	@Test
 public void test() {
 	LoginPage1 lp = new LoginPage1(driver);
-	lp.enterPassword("vijay");
-	lp.enterPassword("1234");
+	lp.enterPassword("student");
+	lp.enterPassword("Password123");
 	lp.clickSubmit();
 }
 }
